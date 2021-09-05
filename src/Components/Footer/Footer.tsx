cdd42e7a27ff0cf0cad2af8ec70c191d4fd1f72a
micolka <micolkaby@gmail.com>
2021-09-05 20:08:35 +0300

    feat: implement footer and header, fix mentor's comments

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,7 +5,41 @@ import React from 'react';
 
 const Footer: FunctionComponent = () => (
   <div className="footer">
-    <div className="footer-container">Fotter</div>
+    <div className="footer-container">
+      {' '}
+      <div className="footer-text">2021</div>
+      <a
+        className="link-footer"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/dinosavr">
+        <img src="src/assets/png/github-logo.png" alt="github"></img>
+        <span>dinosavr</span>
+      </a>
+      <a
+        className="link-footer"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/micolka">
+        <img src="src/assets/png/github-logo.png" alt="github"></img>
+        <span>micolka</span>
+      </a>
+      <a
+        className="link-footer"
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/pashasok">
+        <img src="src/assets/png/github-logo.png" alt="github"></img>
+        <span>pashasok</span>
+      </a>
+      <a
+        className="school-link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://rs.school/js/">
+        <img src="https://rs.school/images/rs_school_js.svg" alt="rs_school" />
+      </a>
+    </div>
   </div>
 );
 
