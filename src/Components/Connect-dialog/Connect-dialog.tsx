65fb6daef6e1db47d7d5685d19e6eb779c4f3b46
micolka <micolkaby@gmail.com>
2021-09-08 17:08:26 +0300

    feat: implement connect-dialog

--- src/Components/Connect-dialog/Connect-dialog.tsx
+++ src/Components/Connect-dialog/Connect-dialog.tsx
@@ -1,15 +1,18 @@
 import './Connect-dialog.scss';
 
-import { FormControl, Input, InputLabel } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-// import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Slide from '@material-ui/core/Slide';
-// import Switch from '@material-ui/core/Switch';
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  FormControl,
+  Input,
+  InputLabel,
+  Slide,
+  Switch,
+  TextField,
+} from '@material-ui/core';
 import { TransitionProps } from '@material-ui/core/transitions';
 import type { FunctionComponent } from 'react';
 import React from 'react';
@@ -26,12 +29,17 @@ const ConnectDialog: FunctionComponent = () => {
   const [name, setName] = React.useState('');
   const [lastName, setLastName] = React.useState('');
   const [jobPosition, setJobPosition] = React.useState('');
+  const [isObserver, setObserver] = React.useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
+    setName('');
+    setLastName('');
+    setJobPosition('');
+    setObserver(false);
     setOpen(false);
   };
 
@@ -47,40 +55,62 @@ const ConnectDialog: FunctionComponent = () => {
         onClose={handleClose}
         aria-labelledby="alert-dialog-slide-title"
         className="dialog-wrapper">
-        <DialogTitle id="alert-dialog-slide-title">{'Connect to lobby'}</DialogTitle>
         <DialogContent className="dialog-content">
-          <FormControl>
-            <InputLabel htmlFor="component-simple">Your first name:</InputLabel>
-            <Input
-              id="component-simple"
-              value={name}
-              onChange={(event) => {
-                setName(event.target.value);
-              }}
-            />
-          </FormControl>
-
-          <FormControl>
-            <InputLabel htmlFor="component-simple">Your last name:</InputLabel>
-            <Input
-              id="component-simple"
-              value={lastName}
-              onChange={(event) => {
-                setLastName(event.target.value);
-              }}
-            />
-          </FormControl>
-
-          <FormControl>
-            <InputLabel htmlFor="component-simple">Your job position:</InputLabel>
-            <Input
-              id="component-simple"
-              value={jobPosition}
-              onChange={(event) => {
-                setJobPosition(event.target.value);
-              }}
-            />
-          </FormControl>
+          <div className="title-wrapper">
+            <span className="large-text">Connect to lobby</span>
+            <div className="switch-container">
+              <span className="switch-text">Connect as observer</span>
+              <Switch
+                checked={isObserver}
+                onChange={() => {
+                  setObserver(!isObserver);
+                }}
+                color="primary"
+                name="observer"
+              />
+            </div>
+          </div>
+          <div className="input-wrapper">
+            <FormControl required>
+              <InputLabel htmlFor="component-simple">Your first name:</InputLabel>
+              <Input
+                id="component-simple"
+                value={name}
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="component-simple">Your last name:</InputLabel>
+              <Input
+                id="component-simple"
+                value={lastName}
+                onChange={(event) => {
+                  setLastName(event.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="component-simple">Your job position:</InputLabel>
+              <Input
+                id="component-simple"
+                value={jobPosition}
+                onChange={(event) => {
+                  setJobPosition(event.target.value);
+                }}
+              />
+            </FormControl>
+            <div className="choose-file-wrapper">
+              <TextField label="Image:" variant="outlined" value="Choose file" disabled />
+              <Button variant="contained" color="primary">
+                Button
+              </Button>
+            </div>
+            <Avatar alt="Remy Sharp" src="">
+              RS
+            </Avatar>
+          </div>
         </DialogContent>
         <DialogActions>
           <Button variant="contained" onClick={handleClose} color="primary">
