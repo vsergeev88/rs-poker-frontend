6ff49216d1cc9b687991aee732fa86594290eb0e
Alex Sidorov <promoabys@gmail.com>
2021-09-12 22:29:03 +0300

    feat: style lobby page

--- src/Pages/Lobby-page/Lobby-page.tsx
+++ src/Pages/Lobby-page/Lobby-page.tsx
@@ -3,26 +3,28 @@ import './Lobby-page.scss';
 import {
   Box,
   Button,
-  Card,
-  CardActions,
-  CardContent,
+  // Card,
+  // CardActions,
+  // CardContent,
   Container,
   Grid,
-  IconButton,
-  makeStyles,
+  // IconButton,
+  // makeStyles,
   Switch,
   TextField,
   Typography,
 } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+// import AddIcon from '@material-ui/icons/Add';
+// import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import EditIcon from '@material-ui/icons/Edit';
 import type { FunctionComponent, HTMLAttributes } from 'react';
 import React from 'react';
 
+import Card from '../../Components/card';
+import Issue from '../../Components/issue';
 import PlayerCard from '../../Components/player-card';
-import { playersMockData } from '../../data/game';
+import { cardMockData2, issueMockData, playersMockData } from '../../data/game';
 
 /* const [state, setState] = React.useState({
   gilad: true,
@@ -43,23 +45,11 @@ const startGame = () => {
 const cancelGame = () => {
   console.log('cancelGame');
 };
-const useStyles = makeStyles({
-  root: {
-    minWidth: 275,
-  },
-  h1: {
-    fontSize: 16,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
 
 const LobbyPage: FunctionComponent<HTMLAttributes<HTMLDivElement>> = () => {
-  const classes = useStyles();
-  // const bull = <span className={classes.bullet}>•</span>;
+  // const classes = useStyles();
   return (
-    <Container className="lobby-page">
+    <Box className="lobby-page">
       <Container className="lobby-page-wrapper">
         <Typography variant="h4" component="h1" gutterBottom>
           Spring 23 planning (issues 13, 533, 5623, 3252, 6623, ...)
@@ -130,166 +120,127 @@ const LobbyPage: FunctionComponent<HTMLAttributes<HTMLDivElement>> = () => {
             ))}
           </Box>
         </Box>
-        <Typography variant="h5" component="h2">
-          Issues:
-        </Typography>
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Issues 542
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-              Low priority
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="Delete">
-              <DeleteIcon color="primary" />
-            </IconButton>
-            <IconButton aria-label="Delete">
-              <EditIcon />
-            </IconButton>
-          </CardActions>
-        </Card>
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Create New Issues
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="Add">
-              <AddIcon color="primary" fontSize="large" />
-            </IconButton>
-          </CardActions>
-        </Card>
-        <Typography variant="h5" component="h2">
-          Game settings:
-        </Typography>
-        {/* <FormControl component="fieldset">
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Switch checked={state.gilad} onChange={handleChange} name="gilad" />
-              }
-              label="Gilad Gray"
-            />
-            <FormControlLabel
-              control={
-                <Switch checked={state.jason} onChange={handleChange} name="jason" />
-              }
-              label="Jason Killian"
-            />
-            <FormControlLabel
-              control={
-                <Switch checked={state.antoine} onChange={handleChange} name="antoine" />
-              }
-              label="Antoine Llorca"
-            />
-          </FormGroup>
-          <FormHelperText>Be careful</FormHelperText>
-        </FormControl> */}
-        <Grid container spacing={3}>
-          <Grid item xs={8}>
-            <Typography variant="h6" component="h2">
-              Scram master as player:
-            </Typography>
+        <Box className="issues section" component="section">
+          <Typography
+            className="label-issues text-center"
+            variant="h5"
+            component="h2"
+            gutterBottom>
+            Issues:
+          </Typography>
+          <Box className="cards-wrapper mb-20">
+            <Issue issue={issueMockData[0]} isLobby={true} />
+            <Issue issue={issueMockData[1]} isLobby={false} />
+            <Issue issue={issueMockData[2]} isLobby={false} />
+          </Box>
+        </Box>
+        <Container className="setting section" component="section">
+          <Typography
+            className="setting-issues text-center"
+            variant="h5"
+            component="h2"
+            gutterBottom>
+            Game settings:
+          </Typography>
+          <Grid className="setting-grid" container spacing={3}>
+            <Grid item xs={8}>
+              <Typography variant="h6" component="h3">
+                Scram master as player:
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Switch
+                color="primary"
+                name="checkedB"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="h6" component="h3">
+                Changing card in round end:
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Switch
+                color="primary"
+                name="checkedB"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="h6" component="h3">
+                Is timer needed:
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <Switch
+                color="primary"
+                name="checkedB"
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6" component="h3">
+                Score type:
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <TextField
+                id="setting-scrore-type"
+                label="story point"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6" component="h3">
+                Score type (Short):
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <TextField id="setting-scrore-type" label="SP" variant="outlined" />
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="h6" component="h3">
+                Round time:
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <TextField
+                id="time"
+                label="Round time"
+                type="time"
+                defaultValue="00:01:30"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                inputProps={{
+                  step: 10, // 5 min
+                }}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={4}>
-            <Switch
-              color="primary"
-              name="checkedB"
-              inputProps={{ 'aria-label': 'primary checkbox' }}
-            />
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="h6" component="h2">
-              Changing card in round end:
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Switch
-              color="primary"
-              name="checkedB"
-              inputProps={{ 'aria-label': 'primary checkbox' }}
-            />
-          </Grid>
-          <Grid item xs={8}>
-            <Typography variant="h6" component="h2">
-              Is timer needed:
-            </Typography>
-          </Grid>
-          <Grid item xs={3}>
-            <Switch
-              color="primary"
-              name="checkedB"
-              inputProps={{ 'aria-label': 'primary checkbox' }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="h2">
-              Score type:
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField id="setting-scrore-type" label="story point" variant="outlined" />
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="h2">
-              Score type (Short):
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField id="setting-scrore-type" label="SP" variant="outlined" />
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="h6" component="h2">
-              Round time:
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              id="time"
-              label="Round time"
-              type="time"
-              defaultValue="00:01:30"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              inputProps={{
-                step: 10, // 5 min
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Typography variant="h5" component="h2">
-          Add card values:
-        </Typography>
-        <Card className={classes.root}>
-          <CardContent>
-            <Typography className={classes.pos} color="textSecondary">
-              12
-            </Typography>
-            <Typography variant="h5" component="h2">
-              SP
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="Delete">
-              <DeleteIcon color="primary" />
-            </IconButton>
-            <IconButton aria-label="Delete">
-              <EditIcon />
-            </IconButton>
-          </CardActions>
-        </Card>
-        <Card className={classes.root}>
-          <CardContent>
-            <AddCircleOutlineIcon color="primary" fontSize="large" />
-          </CardContent>
-        </Card>
+        </Container>
+        <Container className="add-cards section" component="section">
+          <Typography
+            className="label-add-cards text-center"
+            variant="h5"
+            component="h2"
+            gutterBottom>
+            Add card values:
+          </Typography>
+          <Box className="cards-wrapper justify-content-start mb-20">
+            {cardMockData2.map((el, key) => (
+              <Card
+                propCardValue={el.toString()}
+                shortScoreType={'SP'}
+                allowEdit={true}
+                key={key}
+              />
+            ))}
+          </Box>
+        </Container>
       </Container>
-    </Container>
+    </Box>
   );
 };
 
