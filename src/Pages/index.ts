cd788272b059b1f6efde18b6f9e30dc6c08ae9d5
Dasha Ostrikova <dashka.ostrikova@gmail.com>
2021-09-30 19:27:13 +0300

    feat: add game-result

--- src/Pages/index.ts
+++ src/Pages/index.ts
@@ -1,6 +1,7 @@
 import GamePage from './Game-page';
+import GameResult from './Game-result';
 import LobbyPage from './Lobby-page';
 import MainPage from './Main-page';
 import TestPage from './Test-page';
 
-export { GamePage, LobbyPage, MainPage, TestPage };
+export { GamePage, GameResult, LobbyPage, MainPage, TestPage };
